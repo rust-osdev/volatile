dd5cfaf2b5c0c6ce7b94cd70c41c38699fb76599
Philipp Oppermann <dev@phil-opp.com>
2023-01-20 11:11:34 +0100

    Provide a default `Access` implementation for `Readable`
    Specifies that readable access stay readable when shared.

--- src/access.rs
+++ src/access.rs
@@ -9,11 +9,20 @@ pub trait Access: Copy + Default {
 }
 
 /// Helper trait that is implemented by [`ReadWrite`] and [`ReadOnly`].
-pub trait Readable: Access {
+pub trait Readable: Copy + Default {
+    type RestrictShared: Readable + Access;
+
     /// Ensures that this trait cannot be implemented outside of this crate.
     fn _private() -> _Private {
         _Private
     }
+}
+
+impl<T> Access for T
+where
+    T: Readable + Default + Copy,
+{
+    type RestrictShared = <T as Readable>::RestrictShared;
 }
 
 /// Helper trait that is implemented by [`ReadWrite`] and [`WriteOnly`].
@@ -27,19 +36,17 @@ pub trait Writable: Access {
 /// Zero-sized marker type for allowing both read and write access.
 #[derive(Debug, Default, Copy, Clone)]
 pub struct ReadWrite;
-impl Access for ReadWrite {
+impl Readable for ReadWrite {
     type RestrictShared = ReadOnly;
 }
-impl Readable for ReadWrite {}
 impl Writable for ReadWrite {}
 
 /// Zero-sized marker type for allowing only read access.
 #[derive(Debug, Default, Copy, Clone)]
 pub struct ReadOnly;
-impl Access for ReadOnly {
+impl Readable for ReadOnly {
     type RestrictShared = ReadOnly;
 }
-impl Readable for ReadOnly {}
 
 /// Zero-sized marker type for allowing only write access.
 #[derive(Debug, Default, Copy, Clone)]
